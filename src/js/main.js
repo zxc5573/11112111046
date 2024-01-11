4b537da56095d0cbaab37b58b23aca09cacdec45
zxc5573 <QAQ0936648682@email.com>
2024-01-11 14:39:16 +0800

    01

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
